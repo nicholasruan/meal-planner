6e66a3cb212e277fdcb1e7633ed55d909676d51d
chrisyuaners <chrisyuaners@gmail.com>
2019-09-09 16:15:02 -0400

    added combine reducers

--- meal-planner-client/src/index.js
+++ meal-planner-client/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import userReducer from './reducers/userReducer';
+import mealReducer from './reducers/mealReducer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  users: userReducer,
+  meals: mealReducer
+})
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
