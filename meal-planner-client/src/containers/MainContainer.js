27cb772ae4d92794a313225e8df15db1a82d1732
chrisyuaners <chrisyuaners@gmail.com>
2019-09-09 12:07:01 -0400

    setting up redux and added signup

---
+++ meal-planner-client/src/containers/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUp from '../components/SignUp'
+
+class MainContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <SignUp />
+      </div>
+    )
+  }
+}
+
+export default MainContainer
