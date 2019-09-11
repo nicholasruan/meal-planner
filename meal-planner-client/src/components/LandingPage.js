c3c1cb8aa90721230be473e4a555cdd06fe94dce
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-09-11 13:21:19 -0400

    fixed scrolling

--- meal-planner-client/src/components/LandingPage.js
+++ meal-planner-client/src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef} from 'react'
 import { Link } from 'react-router-dom'
-import { TweenMax } from 'gsap';
+import { TweenMax, Power3 } from 'gsap';
 import '../App.css';
 
 const LandingPage = (props) => {
@@ -9,15 +9,17 @@ const LandingPage = (props) => {
   let linksElement = useRef(null);
 
   useEffect(() => {
-    TweenMax.to( titleElement, .8,{ opacity: 1, y: 250, delay: .8 })
-    TweenMax.to( bannerElement, .6,{ x: 3000, delay: 1.1 })
+    console.log(bannerElement.offsetWidth);
+    let test = bannerElement.offsetWidth * 500;
+    TweenMax.to( titleElement, .8,{ opacity: 1, delay: .8 })
+    TweenMax.to( bannerElement, .6,{ width: test, delay: 1.1, ease:Power3.easeInOut})
     TweenMax.to( linksElement, .7,{ opacity: 1, delay: 2.0 })
   }, []);
 
 
   return (
-    <div>
-      <h1 id="landing-title" ref={el => {titleElement = el}}>MEAL PLANNER</h1>
+    <div id="landing-page">
+      <h1 id="landing-title" ref={el => {titleElement = el}}>MEAL MATE</h1>
       <div id="link-container" ref={el => {linksElement = el}}>
       <Link to="/login"><h2 id="link-login">Login</h2></Link>
       <Link to="/signup"><h2 id="link-signup">Sign Up</h2></Link>
