56a684cec81c228c50a6e34f83d0304468a8d0fe
chrisyuaners <chrisyuaners@gmail.com>
2019-09-15 15:21:04 -0400

    meal form set up;

--- meal-planner-client/src/components/RecipeCard.js
+++ meal-planner-client/src/components/RecipeCard.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 class RecipeCard extends React.Component {
 	render() {
 		return (
 			<div onClick={this.props.customClickEvent}>
-				<Card>
+				<Card className={this.props.selectedMeal === this.props.id ? "form-mode" : null}>
 			    <Card.Img variant="top" className="food-image" src={this.props.imageurl} />
 			    <Card.Body>
 			      <Card.Text className="card-text">
