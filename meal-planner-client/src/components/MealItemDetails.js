3d76f05af468c616f0a9dcb87d25bf5ebaa4008b
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-09-16 23:10:00 -0400

    delete now renders the planner page

--- meal-planner-client/src/components/MealItemDetails.js
+++ meal-planner-client/src/components/MealItemDetails.js
@@ -4,46 +4,48 @@ import Button from 'react-bootstrap/Button';
 import RecipeDetails from './RecipeDetails';
 import axios from 'axios';
 
-function handleDelete(docId){
-	axios.request({
-		method: 'DELETE',
-		url: `https://us-central1-meal-planner-164c3.cloudfunctions.net/app/deleteMeal`,
-		data: {
-			'uid': localStorage.user_id,
-			'docId': docId
-		},
-	}).then((response) => {
+class MealItemDetails extends React.Component{
+	handleDelete = (docId) => {
+		axios.request({
+			method: 'DELETE',
+			url: `https://us-central1-meal-planner-164c3.cloudfunctions.net/app/deleteMeal`,
+			data: {
+				'uid': localStorage.user_id,
+				'docId': docId
+			},
+		}).then((response) => {
+			console.log('meal deleted');
+			this.props.removeMealFromState(docId);
+		}).catch((error) => {
+			console.log(error);
+			alert("unable to delete meal");
+		})
+	}
 
-	}).catch((error) => {
-		console.log(error);
-		alert("unable to delete meal");
-	})
-}
-
-function MealItemDetails(props) {
-	return (
-		<Modal
-			{...props}
-			size="lg"
-			aria-labelledby="contained-modal-title-vcenter"
-			centered >
-			<Modal.Header closeButton>
-			</Modal.Header>
-			<Modal.Body>
-				<p>{props.docId}</p>
-					<RecipeDetails
-						recipeId={props.recipeId}
-						menuItemMode={true}
-					/>
-				<div className="delete-button">
-					<Button className="btn btn-danger" onClick={handleDelete(props.docId)}>Delete</Button>
-				</div>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button onClick={props.onHide}>Close</Button>
-			</Modal.Footer>
-		</Modal>
-	)
+	render() {
+		return (
+			<Modal
+				{...this.props}
+				size="lg"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered >
+				<Modal.Header closeButton>
+				</Modal.Header>
+				<Modal.Body>
+						<RecipeDetails
+							recipeId={this.props.recipeId}
+							menuItemMode={true}
+						/>
+					<div className="delete-button">
+						<Button className="btn btn-danger" onClick={() => this.handleDelete(this.props.docId)}>Delete</Button>
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={this.props.onHide}>Close</Button>
+				</Modal.Footer>
+			</Modal>
+		)
+	}
 }
 
 export default MealItemDetails
